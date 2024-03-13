98ba29120ffca9d29cbc95da0c1934e23a823570
zhangm365 <mzhang2048@gmail.com>
2024-03-14 01:09:13 +0800

    update cmu 02-insert.sql

--- DB/cmu15445/lecture/02-insert.sql
+++ DB/cmu15445/lecture/02-insert.sql
@@ -18,28 +18,23 @@ CREATE TABLE enrolled (
     grade CHAR(1)
 );
 
-insert into student values (53666, 'Kanye', 'kanye@cs', 44, 4.0),
-        (53688, 'Bieber', 'jbieber@cs', 27, 3.9),
-        (53655, 'Tupac', 'shakur@cs', 25, 3.5)
-;
+insert into student values 
+    (53666, 'RZA', 'rza@cs', 44, 4.0),
+    (53688, 'Bieber', 'jbieber@cs', 27, 3.9),
+    (53655, 'Tupac', 'shakur@cs', 25, 3.5);
 
-insert into student values (53633, 'John', 'john@cs', 24, 4.0),
-        (53611, 'Apple', 'apple@cs', 30, 3.9),
-        (53600, 'Google', 'google@cs', 25, 3.5)
-;
+insert into course values 
+    ('15-445', 'Database Systems'),
+    ('15-721', 'Advanced Database Systems'),
+    ('15-826', 'Data Mining'),
+    ('15-799', 'Special Topics in Databases');
 
-insert into course values ('15-445', 'Database Systems'),
-        ('15-721', 'Advanced Database Systems'),
-        ('15-826', 'Data Mining'),
-        ('15-799', 'Special Topics in Databases')
-;
-
-insert into enrolled values (53666, '15-445', 'C'),
-        (53688, '15-721', 'A'),
-        (53688, '15-826', 'B'),
-        (53655, '15-445', 'B'),
-        (53666, '15-721', 'C')
-;
+insert into enrolled values 
+    (53666, '15-445', 'C'),
+    (53688, '15-721', 'A'),
+    (53688, '15-826', 'B'),
+    (53655, '15-445', 'B'),
+    (53666, '15-721', 'C');
 
 
 -- NESTED QUERIES
