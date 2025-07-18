33d1403f79812a239e6ebe0fbd6b6052a0e54512
zhangm365 <mzhang2048@gmail.com>
2025-07-18 12:54:25 +0800

    update.

--- CPP/code/Add_main.cpp
+++ CPP/code/Add_main.cpp
@@ -1,25 +1,16 @@
 
-
-
-
 #include <iostream>
 
 extern "C"
 {
     #include "Add.h"  
 }
-  
-
 
 int main()
 {
-
     int x = 2, y = 3;
     int res = add(x, y);
 
     printf("res = %d\n", res);
-
     return 0;
-
-
 }
