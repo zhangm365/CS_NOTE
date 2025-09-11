1c1910787f518d9b252362bd99fef2d86d1de484
zhangm365 <mzhang2048@gmail.com>
2025-09-11 23:32:48 +0800

    update

--- CPP/code/src/examples/condition_variable.cpp
+++ CPP/code/src/examples/condition_variable.cpp
@@ -5,16 +5,17 @@
 #include <condition_variable>
 #include <chrono>
 
-std::mutex              mtx;
+volatile int g_val = 0; // 全局变量，供线程间通信使用
+bool ready = false;  // 资源就绪标志
+std::mutex mtx;
 std::condition_variable cv;
-bool                    ready = false;  // 资源就绪标志
 
 // 消费者：等待资源
 void consumer() {
-    std::unique_lock<std::mutex> lk(mtx);
+    std::unique_lock lk(mtx);
     std::cout << "[Consumer] 等待资源...\n";
     // 使用带谓词的 wait，防止虚假唤醒或错过通知
-    cv.wait(lk, []{ return ready; });
+    cv.wait(lk, []{ return (ready && (g_val == 1)); });
     // 唤醒后一定拿到了锁，且 ready == true
     std::cout << "[Consumer] 获得资源，开始消费。\n";
 }
@@ -24,8 +25,9 @@ void producer() {
     // 模拟生产耗时
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-    std::scoped_lock<std::mutex> slk(mtx);
+    std::scoped_lock slk(mtx);
     ready = true;
+    g_val += 1;
     std::cout << "[Producer] 资源已准备好，发出通知。\n";
     
     cv.notify_one();  // 通知一个等待线程
