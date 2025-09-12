a0f718f982b95b9bd8ea06b5fd5d94b6226258d1
zhangm365 <mzhang2048@gmail.com>
2025-09-12 15:18:07 +0800

    update include dir

--- OS/OSTEP/my-code/practice/ch30-threads-cv/condition_variable.c
+++ OS/OSTEP/my-code/practice/ch30-threads-cv/condition_variable.c
@@ -1,22 +1,27 @@
 
 #include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include <unistd.h>
 #include <assert.h>
+#include "common_threads.h"
+
+volatile int val = 0;
 
 void *child(void *arg) {
     printf("child\n");
+    val = 1;
     return NULL;
 }
 
 int main() {
     printf("parent begin\n");
     pthread_t c;
-    assert(pthread_create(&c, NULL, child, NULL) == 0);
-    assert(pthread_join(c, NULL) == 0);
+    Pthread_create(&c, NULL, child, NULL);
+    Pthread_join(c, NULL);
 
-    printf("parent end\n");
+    printf("parent end, the val = %d\n", val);
     return 0;
 }
 
